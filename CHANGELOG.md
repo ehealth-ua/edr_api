086584a00abcc45c971cc50aa9ec0ebf06dc786e
Alex Kovalevych <AlexKovalevych@users.noreply.github.com>
2019-03-27 12:14:38 +0200

    [ci skip] release: 0.0.1 (#9)

---
+++ CHANGELOG.md
@@ -0,0 +1,23 @@
+# Change Log
+
+All notable changes to this project will be documented in this file.
+See [Conventional Commits](Https://conventionalcommits.org) for commit guidelines.
+
+<!-- changelog -->
+
+## [0.0.1](https://github.com/edenlabllc/edr_api/compare/0.0.1...0.0.1) (2019-3-27)
+
+
+
+
+### Features:
+
+* Json validation added (#1)
+
+### Bug Fixes:
+
+* Fix naming (#6)
+
+* add github token (#4)
+
+* edr responses interpretation (#2)
--- config/config.exs
+++ config/config.exs
@@ -2,21 +2,27 @@
 # and its dependencies with the aid of the Mix.Config module.
 use Mix.Config
 
-# By default, the umbrella project as well as each child
-# application will require this configuration file, as
-# configuration and dependencies are shared in an umbrella
-# project. While one could configure all applications here,
-# we prefer to keep the configuration of each individual
-# child application in their own app, but all other
-# dependencies, regardless if they belong to one or multiple
-# apps, should be configured in the umbrella to avoid confusion.
+config :git_ops,
+  mix_project: EdrApi.Umbrella.Mixfile,
+  changelog_file: "CHANGELOG.md",
+  repository_url: "https://github.com/edenlabllc/edr_api/",
+  types: [
+    # Makes an allowed commit type called `tidbit` that is not
+    # shown in the changelog
+    tidbit: [
+      hidden?: true
+    ],
+    # Makes an allowed commit type called `important` that gets
+    # a section in the changelog with the header "Important Changes"
+    important: [
+      header: "Important Changes"
+    ]
+  ],
+  # Instructs the tool to manage your mix version in your `mix.exs` file
+  # See below for more information
+  manage_mix_version?: true,
+  # Instructs the tool to manage the version in your README.md
+  # Pass in `true` to use `"README.md"` or a string to customize
+  manage_readme_version: "README.md"
+
 import_config "../apps/*/config/config.exs"
-
-# Sample configuration (overrides the imported configuration above):
-#
-#     config :logger, :console,
-#       level: :info,
-#       format: "$date $time [$level] $metadata$message\n",
-#       metadata: [:user_id]
-
-import_config "#{Mix.env()}.exs"
