8bf0b5bf1ca1072e4dc8c6c28296b9ef368748dc
Alex Kovalevych <AlexKovalevych@users.noreply.github.com>
2019-04-22 15:18:43 +0300

    feat: get legal entity defailt info rpc (#12)

--- apps/edr_api/lib/edr_api/rpc/rpc.ex
+++ apps/edr_api/lib/edr_api/rpc/rpc.ex
@@ -16,73 +16,73 @@ defmodule EdrApi.Rpc do
       iex> EdrApi.Rpc.legal_entity_by_code("00000000")
       {:ok,
       %{
-        activity_kinds: [
+        "activity_kinds" => [
           %{
             ...
           }
         ],
-        address: %{
-          address: "...",
-          country: "...",
-          parts: %{
-            atu: "...",
-            atu_code: "ATU_CODE",
+        "address" => %{
+          "address" => "...",
+          "country" => "...",
+          "parts" => %{
+            "atu" => "...",
+            "atu_code" => "ATU_CODE",
             ...
           },
           ...
         },
-        assignees: [],
-        authorised_capital: %{...},
-        bankruptcy: nil,
-        branches: [],
-        code: "some code",
-        contacts: %{...},
-        executive_power: nil,
-        founders: [
+        "assignees" => [],
+        "authorised_capital" => %{...},
+        "bankruptcy" => nil,
+        "branches" => [],
+        "code" => "some code",
+        "contacts" => %{...},
+        "executive_power" => nil,
+        "founders" => [
           %{
             ...
           }
         ],
-        founding_document: nil,
-        heads: [
+        "founding_document" => nil,
+        "heads" => [
           %{
             ...
           }
         ],
-        id: 1111111,
-        is_modal_statute: false,
-        management: "...",
-        managing_paper: nil,
-        names: %{
-          display: "Some name",
-          include_olf: 1,
-          name: "...",
-          name_en: "",
-          short: "...",
-          short_en: ""
+        "id" => 1111111,
+        "is_modal_statute" => false,
+        "management" => "...",
+        "managing_paper" => nil,
+        "names" => %{
+          "display" => "Some name",
+          "include_olf" => 1,
+          "name" => "...",
+          "name_en" => "",
+          "short" => "...",
+          "short_en" => ""
         },
-        object_name: "...",
-        olf_code: "...",
-        olf_name: "...",
-        open_enforcements: ["2000-01-01", ...],
-        predecessors: [],
-        prev_registration_end_term: nil,
-        primary_activity_kind: %{
+        "object_name" => "...",
+        "olf_code" => "...",
+        "olf_name" => "...",
+        "open_enforcements" => ["2000-01-01", ...],
+        "predecessors" => [],
+        "prev_registration_end_term" => nil,
+        "primary_activity_kind" => %{
           ...
         },
-        registration: %{
+        "registration" => %{
           ...
         },
-        registrations: [
+        "registrations" => [
           %{
             ...
           },
           ...
         ],
-        state: 1,
-        state_text: "зареєстровано",
-        termination: nil,
-        termination_cancel: nil
+        "state" => 1,
+        "state_text" => "зареєстровано",
+        "termination" => nil,
+        "termination_cancel" => nil
       }}
 
       iex> EdrApi.Rpc.legal_entity_by_code("00000000")
@@ -112,73 +112,73 @@ defmodule EdrApi.Rpc do
       iex> EdrApi.Rpc.legal_entity_by_pasport("ББ000000")
       {:ok,
       %{
-        activity_kinds: [
+        "activity_kinds" => [
           %{
             ...
           }
         ],
-        address: %{
-          address: "...",
-          country: "...",
-          parts: %{
-            atu: "...",
-            atu_code: "ATU_CODE",
+        "address" => %{
+          "address" => "...",
+          "country" => "...",
+          "parts" => %{
+            "atu" => "...",
+            "atu_code" => "ATU_CODE",
             ...
           },
           ...
         },
-        assignees: [],
-        authorised_capital: %{...},
-        bankruptcy: nil,
-        branches: [],
-        code: "some code",
-        contacts: %{...},
-        executive_power: nil,
-        founders: [
+        "assignees" => [],
+        "authorised_capital" => %{...},
+        "bankruptcy" => nil,
+        "branches" => [],
+        "code" => "some code",
+        "contacts" => %{...},
+        "executive_power" => nil,
+        "founders" => [
           %{
             ...
           }
         ],
-        founding_document: nil,
-        heads: [
+        "founding_document" => nil,
+        "heads" => [
           %{
             ...
           }
         ],
-        id: 1111111,
-        is_modal_statute: false,
-        management: "...",
-        managing_paper: nil,
-        names: %{
-          display: "Some name",
-          include_olf: 1,
-          name: "...",
-          name_en: "",
-          short: "...",
-          short_en: ""
+        "id" => 1111111,
+        "is_modal_statute" => false,
+        "management" => "...",
+        "managing_paper" => nil,
+        "names" => %{
+          "display" => "Some name",
+          "include_olf" => 1,
+          "name" => "...",
+          "name_en" => "",
+          "short" => "...",
+          "short_en" => ""
         },
-        object_name: "...",
-        olf_code: "...",
-        olf_name: "...",
-        open_enforcements: ["2000-01-01", ...],
-        predecessors: [],
-        prev_registration_end_term: nil,
-        primary_activity_kind: %{
+        "object_name" => "...",
+        "olf_code" => "...",
+        "olf_name" => "...",
+        "open_enforcements" => ["2000-01-01", ...],
+        "predecessors" => [],
+        "prev_registration_end_term" => nil,
+        "primary_activity_kind" => %{
           ...
         },
-        registration: %{
+        "registration" => %{
           ...
         },
-        registrations: [
+        "registrations" => [
           %{
             ...
           },
           ...
         ],
-        state: 1,
-        state_text: "зареєстровано",
-        termination: nil,
-        termination_cancel: nil
+        "state" => 1,
+        "state_text" => "зареєстровано",
+        "termination" => nil,
+        "termination_cancel" => nil
       }}
 
       iex> EdrApi.Rpc.legal_entity_by_pasport("ББ000000")
@@ -200,14 +200,124 @@ defmodule EdrApi.Rpc do
     end
   end
 
-  defp search_legal_entity(params) do
+  @doc """
+  Search legal entity by passport or EDRPOU
+
+  ## Examples
+
+      iex> EdrApi.Rpc.search_legal_entity(%{passport: "ББ000000"})
+      {:ok, %{
+        "url" => "https://example.com",
+        "id" => 123456,
+        "state" => 1,
+        "state_text" => "зареєстровано",
+        "passport" => "АА111111",
+        "name" => "TEST"
+      }}
+
+      iex> EdrApi.Rpc.legal_entity_by_pasport(%{passport: "ББ000000"})
+      {:error,
+        %{
+          "body" => %{"errors" => [%{"code" => 2, "message" => "Invalid token."}]},
+          "status_code" => 401
+        }}
+  """
+  @spec search_legal_entity(params :: map()) ::
+          {:ok, map()} | {:error, binary()} | {:error, atom()} | {:error, map()}
+  def search_legal_entity(params) do
     case @edr_api.search_legal_entity(params) do
       {:ok, response} -> validate_response(response)
       error -> error
     end
   end
 
-  defp get_legal_entity_detailed_info(id) do
+  @doc """
+  Get legal entity detailed information by id
+
+  ## Examples
+
+      iex> EdrApi.Rpc.get_legal_entity_detailed_info("1111111")
+      {:ok,
+      %{
+        "activity_kinds" => [
+          %{
+            ...
+          }
+        ],
+        "address" => %{
+          "address" => "...",
+          "country" => "...",
+          "parts" => %{
+            "atu" => "...",
+            "atu_code" => "ATU_CODE",
+            ...
+          },
+          ...
+        },
+        "assignees" => [],
+        "authorised_capital" => %{...},
+        "bankruptcy" => nil,
+        "branches" => [],
+        "code" => "some code",
+        "contacts" => %{...},
+        "executive_power" => nil,
+        "founders" => [
+          %{
+            ...
+          }
+        ],
+        "founding_document" => nil,
+        "heads" => [
+          %{
+            ...
+          }
+        ],
+        "id" => 1111111,
+        "is_modal_statute" => false,
+        "management" => "...",
+        "managing_paper" => nil,
+        "names" => %{
+          "display" => "Some name",
+          "include_olf" => 1,
+          "name" => "...",
+          "name_en" => "",
+          "short" => "...",
+          "short_en" => ""
+        },
+        "object_name" => "...",
+        "olf_code" => "...",
+        "olf_name" => "...",
+        "open_enforcements" => ["2000-01-01", ...],
+        "predecessors" => [],
+        "prev_registration_end_term" => nil,
+        "primary_activity_kind" => %{
+          ...
+        },
+        "registration" => %{
+          ...
+        },
+        "registrations" => [
+          %{
+            ...
+          },
+          ...
+        ],
+        "state" => 1,
+        "state_text" => "зареєстровано",
+        "termination" => nil,
+        "termination_cancel" => nil
+      }}
+
+      iex> EdrApi.Rpc.get_legal_entity_detailed_info("1111111")
+      {:error,
+        %{
+          "body" => %{"errors" => [%{"code" => 2, "message" => "Invalid token."}]},
+          "status_code" => 401
+        }}
+  """
+  @spec get_legal_entity_detailed_info(id :: binary()) ::
+          {:ok, map()} | {:error, binary()} | {:error, atom()} | {:error, map()}
+  def get_legal_entity_detailed_info(id) do
     case @edr_api.legal_entity_detailed_info(id) do
       {:ok, response} -> validate_response(response)
       error -> error
