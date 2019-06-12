c9c20bf1d7c0261bdd689f9e0a2c98694dd1f866
Alex Kovalevych <AlexKovalevych@users.noreply.github.com>
2019-06-12 12:29:07 +0300

    chore: bump libcluster (#16)

--- apps/edr_api/mix.exs
+++ apps/edr_api/mix.exs
@@ -31,7 +31,7 @@ defmodule EdrApi.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:kube_rpc, "~> 0.1.0"},
+      {:kube_rpc, "~> 0.3.0"},
       {:confex, "~> 3.4"},
       {:httpoison, "~> 1.2"},
       {:jason, "~> 1.1"},
